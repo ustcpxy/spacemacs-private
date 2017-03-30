0927a1f23d059c68a14ab1cbf799f7b6247d0de9
Derek Pan <ustcpxy@gmail.com>
2017-03-30 12:37:06 +0800

    support encrypt given org entry

--- layers/ustcpxy/packages.el
+++ layers/ustcpxy/packages.el
@@ -205,6 +205,25 @@ Each entry is either:
       (setq org-mobile-directory "~/mobile-org")
       (setq org-mobile-files (quote ("~/pkms/gtd")))
       (setq org-mobile-inbox-for-pull "~/mobile-org/inbox.org")
+
+      ;; 加密文章
+      ;; "http://coldnew.github.io/blog/2013/07/13_5b094.html"
+      ;; org-mode 設定
+      (require 'org-crypt)
+
+      ;; 當被加密的部份要存入硬碟時，自動加密回去
+      (org-crypt-use-before-save-magic)
+
+      ;; 設定要加密的 tag 標籤為 secret
+      (setq org-crypt-tag-matcher "secret")
+
+      ;; 避免 secret 這個 tag 被子項目繼承 造成重複加密
+      ;; (但是子項目還是會被加密喔)
+      (setq org-tags-exclude-from-inheritance (quote ("secret")))
+
+      ;; 用於加密的 GPG 金鑰
+      ;; 可以設定任何 ID 或是設成 nil 來使用對稱式加密 (symmetric encryption)
+      (setq org-crypt-key nil)
       )))
 
 (defun ustcpxy/init-yang-mode ()
